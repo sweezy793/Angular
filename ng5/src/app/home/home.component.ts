f4a3e22fb17daaf3fb70f33e7cbe846b28fd1fed
sweezy793 <sarthaks793@gmail.com>
2018-02-05 23:58:28 +0530

    Done with event binding and interpolation

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount:number;
+  btnText="Add an item";
+  goalText:string ="My first life goal";
+  goals=[];
   constructor() { }
 
   ngOnInit() {
+    this.itemCount=this.this.goals.length;
+  }
+  addItem()
+  {
+    this.goals.push(this.goalText);
+    this.goalText='';
+    this.itemCount=this.goals.length;
   }
 
 }
